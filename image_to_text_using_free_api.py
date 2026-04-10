36fbd4d5134b66cb14a2b489b431c22a161e952d
Raptor K <findme@raptor.hk>
2026-04-10 23:03:02 +0800

    Improve code quality
    - change AI model
    - updated the prompt
    - consolidate the model name

--- image_to_text_using_free_api.py
+++ image_to_text_using_free_api.py
@@ -1,19 +1,16 @@
 import base64
 import requests
 import os
-import requests
 import json
-import base64
 from dotenv import load_dotenv
 load_dotenv()
 
-
 url = "https://openrouter.ai/api/v1/chat/completions"
+base_model = "nvidia/nemotron-nano-12b-v2-vl:free"
 headers = {
     "Authorization": f"Bearer {os.getenv("OPENROUTER_API_KEY")}",
     "Content-Type": "application/json"
 }
-
 
 # from image path to base64 converter
 def encode_image_to_base64(image_path):
@@ -32,10 +29,8 @@ def image_to_text_from_url(image_url):
             "content": [
                 {
                     "type": "text",
-                    "text": "Extract all text from this image thoroughly. "
-                            "Do NOT miss any words, headings, lists, faint text, "
-                            "or text near edges. Maintain the original reading order."
-                            "Don't need to give any explanation, just provide the extracted text."
+                    "text": "Describe this photo in detail. Be accurate and helpful. "
+                    		"Don't need to give any explanation, just provide the image description."
                 },
                 {
                     "type": "image_url",
@@ -46,7 +41,7 @@ def image_to_text_from_url(image_url):
     ]
 
     payload = {
-        "model": "amazon/nova-2-lite-v1:free",
+        "model": base_model,
         "messages": messages
     }
 
@@ -70,10 +65,8 @@ def image_to_text_from_base64(image_base64):
             "content": [
                 {
                     "type": "text",
-                    "text": "Extract all text from this image thoroughly. "
-                            "Do NOT miss any words, headings, lists, faint text, "
-                            "or text near edges. Maintain the original reading order."
-                            "Don't need to give any explanation, just provide the extracted text."
+                    "text": "Describe this photo in detail. Be accurate and helpful. "
+                    		"Don't need to give any explanation, just provide the image description."
                 },
                 {
                     "type": "image_url",
@@ -86,7 +79,7 @@ def image_to_text_from_base64(image_base64):
     ]
 
     payload = {
-        "model": "amazon/nova-2-lite-v1:free",
+        "model": base_model,
         "messages": messages
     }
 
@@ -95,23 +88,8 @@ def image_to_text_from_base64(image_base64):
 
     return response.json().get("choices", [])[0].get("message", {}).get("content", "")
 
-
-
 if __name__ == "__main__":
-    
-    # ---- Example 1: From Image URL ----
-    # image_url = "https://media.gettyimages.com/id/1813512682/vector/certificate-diploma-template.jpg?s=612x612&w=gi&k=20&c=sqk_pABwv9CYB4UwaAu3fQdRoILYurBzGok05Fj1JRk=" # Replace with your image URL
-    # text_from_image = image_to_text_from_url(image_url)
-    # print("Text from Image URL:\n", text_from_image)
-
-    # ---- Example 2: From Image Path ----
     image_path = "image.jpg" # Replace with your local image path
     image_base64 = encode_image_to_base64(image_path)
     text_from_image = image_to_text_from_base64(image_base64)
     print("Text from Image Path:\n", text_from_image)
-
-    # Save the extracted text to a file
-    output_file_path = "extracted_text.txt"
-    with open("extracted_text.txt", "w", encoding="utf-8") as output_file:
-        output_file.write(text_from_image)
-    print(f"Extracted text saved to {output_file_path}")
